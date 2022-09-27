96760e5a03cb4c9adf25074181751381cf4f2ad7
EteyeAbasi <eteyendak@gmail.com>
2022-09-27 15:06:14 -0700

    commit initial changes

---
+++ index.js
@@ -0,0 +1,34 @@
+let bentEl = document.getElementById("btn")
+let btnEl = 0
+let btn2El = document.getElementById("btn2")
+let cool = 0
+
+
+function main() {
+    btnEl += 1
+    bentEl.textContent = btnEl
+}
+
+function kit() {
+    btnEl += 2
+    bentEl.textContent = btnEl
+}
+
+function cap() {
+    btnEl += 3
+    bentEl.textContent = btnEl
+}
+
+function pap() {
+    cool += 1
+    btn2El.textContent = cool
+}
+
+function pot() {
+    cool += 2
+    btn2El.textContent = cool
+}
+function eat() {
+    cool += 3
+    btn2El.textContent = cool
+}
